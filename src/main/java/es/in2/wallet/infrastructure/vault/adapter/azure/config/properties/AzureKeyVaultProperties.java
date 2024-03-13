670e507e63f504a84f58df36afaa4fffaae7e15d
AzureAD\JiaboWang <jiabo.wang@in2.es>
2024-03-13 01:47:10 +0100

    fix keyvault name

--- src/main/java/es/in2/wallet/infrastructure/vault/adapter/azure/config/properties/AzureKeyVaultProperties.java
+++ src/main/java/es/in2/wallet/infrastructure/vault/adapter/azure/config/properties/AzureKeyVaultProperties.java
@@ -3,7 +3,7 @@ package es.in2.wallet.infrastructure.vault.adapter.azure.config.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 
-@ConfigurationProperties("azure.keyvault")
+@ConfigurationProperties("azure.key-vault")
 public record AzureKeyVaultProperties(String endpoint) {
 
 
