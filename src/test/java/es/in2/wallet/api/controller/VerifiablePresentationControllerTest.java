c1bf41006c3f7ee1558c93a3691af652d87d88ac
RubenModamioGarcia <ruben.modamio@in2.es>
2024-04-04 12:20:52 +0200

    Update VerifiablePresentationControllerTest.java

--- src/test/java/es/in2/wallet/api/controller/VerifiablePresentationControllerTest.java
+++ src/test/java/es/in2/wallet/api/controller/VerifiablePresentationControllerTest.java
@@ -21,29 +21,8 @@ import static org.mockito.Mockito.when;
 class VerifiablePresentationControllerTest {
     @Mock
     private TurnstileAttestationExchangeService turnstileAttestationExchangeService;
-    @Mock
-    private AttestationExchangeService attestationExchangeService;
     @InjectMocks
     private VerifiablePresentationController verifiablePresentationController;
-    @Test
-    void testCreateVerifiablePresentation() {
-        // Arrange
-        String authorizationToken = "authToken";
-        VcSelectorResponse vcSelectorResponse = VcSelectorResponse.builder().build();
-
-        when(attestationExchangeService.buildVerifiablePresentationWithSelectedVCs(anyString(), eq(authorizationToken), eq(vcSelectorResponse)))
-                .thenReturn(Mono.empty());
-
-        WebTestClient
-                .bindToController(verifiablePresentationController)
-                .build()
-                .post()
-                .uri("/api/v1/vp")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + authorizationToken)
-                .bodyValue(vcSelectorResponse)
-                .exchange()
-                .expectStatus().isCreated();
-    }
 
     @Test
     void testCreateVerifiablePresentationInCborFormat() {
