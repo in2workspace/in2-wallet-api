dc684e1bc0d837577d401054bd835826922aa9aa
Albert Rodríguez <albert.rodriguez@in2.es>
2024-06-06 08:48:59 +0200

    test(broker): refactor test

--- src/test/java/es/in2/wallet/broker/adapter/config/BrokerConfigImplTest.java
+++ src/test/java/es/in2/wallet/broker/adapter/config/BrokerConfigImplTest.java
@@ -3,14 +3,13 @@ package es.in2.wallet.broker.adapter.config;
 import es.in2.wallet.infrastructure.broker.config.BrokerConfig;
 import es.in2.wallet.infrastructure.broker.config.BrokerProperties;
 import es.in2.wallet.infrastructure.core.config.properties.UrlProperties;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,37 +17,67 @@ class BrokerConfigImplTest {
         @Mock
         private BrokerProperties brokerProperties;
 
-
         @InjectMocks
         private BrokerConfig brokerConfig;
 
-        @BeforeEach
-        void setUp() {
-            UrlProperties urlProperties = new UrlProperties("http", "example.com", 8080, "/entities");
-            when(brokerProperties.internalUrl()).thenReturn(urlProperties);
-
-            BrokerProperties.BrokerPathProperties brokerPathProperties = new BrokerProperties.BrokerPathProperties("/entities");
-            when(brokerProperties.paths()).thenReturn(brokerPathProperties);
-
-            brokerConfig = new BrokerConfig(brokerProperties);
-            brokerConfig.init();
-        }
-
         @Test
         void testGetProvider() {
-            when(brokerProperties.provider()).thenReturn("testProvider");
-            assertEquals("testProvider", brokerConfig.getProvider());
+            var expectedProvider = "testProvider";
+            when(brokerProperties.provider()).thenReturn(expectedProvider);
+
+            var result = brokerConfig.getProvider();
+
+            assertThat(result).isEqualTo(expectedProvider);
         }
 
         @Test
         void testGetInternalUrl() {
-            String expectedUrl = "http://example.com:8080";
-            assertEquals(expectedUrl, brokerConfig.getInternalUrl());
+            initWithUrlProperties();
+
+            var expectedUrl = "http://example.com:8080";
+
+            var result = brokerConfig.getInternalUrl();
+
+            assertThat(result).isEqualTo(expectedUrl);
         }
 
         @Test
         void testGetEntitiesPath() {
-            String expectedPath = "/entities";
-            assertEquals(expectedPath, brokerConfig.getEntitiesPath());
+            initWithUrlPropertiesAndBrokerPathProperties();
+
+            var expectedPath = "/entities";
+
+            var result = brokerConfig.getEntitiesPath();
+
+            assertThat(result).isEqualTo(expectedPath);
         }
+
+    private void initWithUrlProperties() {
+        setInternalUrl();
+
+        setAndInitBrokerConfig();
     }
+
+    private void initWithUrlPropertiesAndBrokerPathProperties() {
+        setInternalUrl();
+
+        setPaths();
+
+        setAndInitBrokerConfig();
+    }
+
+    private void setPaths() {
+        BrokerProperties.BrokerPathProperties brokerPathProperties = new BrokerProperties.BrokerPathProperties("/entities");
+        when(brokerProperties.paths()).thenReturn(brokerPathProperties);
+    }
+
+    private void setInternalUrl() {
+        UrlProperties urlProperties = new UrlProperties("http", "example.com", 8080, "/entities");
+        when(brokerProperties.internalUrl()).thenReturn(urlProperties);
+    }
+
+    private void setAndInitBrokerConfig() {
+        brokerConfig = new BrokerConfig(brokerProperties);
+        brokerConfig.init();
+    }
+}
