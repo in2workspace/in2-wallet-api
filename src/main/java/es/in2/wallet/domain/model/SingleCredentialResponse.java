1c81823ead41d6333568c39237cc875104524ed3
Dmitri Sidorov Manko <159002112+dmitriin2@users.noreply.github.com>
2024-09-22 18:26:55 +0200

    add tests

--- src/main/java/es/in2/wallet/domain/model/SingleCredentialResponse.java
+++ src/main/java/es/in2/wallet/domain/model/SingleCredentialResponse.java
@@ -1,7 +1,9 @@
 package es.in2.wallet.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
+@Builder
 public record SingleCredentialResponse(
         @JsonProperty("credential")
         String credential,
