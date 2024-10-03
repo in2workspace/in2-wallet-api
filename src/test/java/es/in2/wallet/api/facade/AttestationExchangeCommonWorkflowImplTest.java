510ee5c00dec2a9ec688ea41fed87e51802d8c98
RubenModamioGarcia <ruben.modamio@in2.es>
2024-10-03 13:23:36 +0200

    update tests

--- src/test/java/es/in2/wallet/api/facade/AttestationExchangeCommonWorkflowImplTest.java
+++ src/test/java/es/in2/wallet/api/facade/AttestationExchangeCommonWorkflowImplTest.java
@@ -22,8 +22,6 @@ import reactor.test.StepVerifier;
 import java.util.List;
 
 import static es.in2.wallet.domain.util.ApplicationUtils.getUserIdFromToken;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,42 +41,43 @@ class AttestationExchangeCommonWorkflowImplTest {
     @InjectMocks
     private AttestationExchangeCommonWorkflowImpl attestationExchangeServiceFacade;
 
-//    @Test
-//    void getSelectableCredentialsRequiredToBuildThePresentationTest() {
-//        try (MockedStatic<ApplicationUtils> ignored = Mockito.mockStatic(ApplicationUtils.class)){
-//            String processId = "123";
-//            String authorizationToken = "authToken";
-//            String qrContent = "qrContent";
-//            String jwtAuthorizationRequest = "authRequest";
-//            AuthorizationRequestOIDC4VP authorizationRequestOIDC4VP = AuthorizationRequestOIDC4VP.builder().scope(List.of("scope1")).redirectUri("redirectUri").state("state").build();
-//            CredentialsBasicInfo credentialsBasicInfo = CredentialsBasicInfo.builder().build();
-//            VcSelectorRequest expectedVcSelectorRequest = VcSelectorRequest.builder().redirectUri("redirectUri").state("state").selectableVcList(List.of(credentialsBasicInfo)).build();
-//            when(authorizationRequestService.getAuthorizationRequestFromVcLoginRequest(processId, qrContent)).thenReturn(Mono.just(jwtAuthorizationRequest));
-//            when(verifierValidationService.verifyIssuerOfTheAuthorizationRequest(processId, jwtAuthorizationRequest)).thenReturn(Mono.just(jwtAuthorizationRequest));
-//            when(authorizationRequestService.getAuthorizationRequestFromJwtAuthorizationRequestJWT(processId, jwtAuthorizationRequest)).thenReturn(Mono.just(authorizationRequestOIDC4VP));
-//            when(getUserIdFromToken(authorizationToken)).thenReturn(Mono.just("userId"));
-//            when(brokerService.getCredentialByCredentialTypeAndUserId(processId, authorizationRequestOIDC4VP.scope().get(0),"userId")).thenReturn(Mono.just("credentialEntity"));
-//            when(dataService.getUserVCsInJson("credentialEntity")).thenReturn(Mono.just(List.of(credentialsBasicInfo)));
-//
-//            StepVerifier.create(attestationExchangeServiceFacade.processAuthorizationRequest(processId, authorizationToken, qrContent))
-//                    .expectNext(expectedVcSelectorRequest)
-//                    .verifyComplete();
-//        }
-//    }
+    @Test
+    void getSelectableCredentialsRequiredToBuildThePresentationTest() {
+        try (MockedStatic<ApplicationUtils> ignored = Mockito.mockStatic(ApplicationUtils.class)){
+            String processId = "123";
+            String authorizationToken = "authToken";
+            String qrContent = "qrContent";
+            String jwtAuthorizationRequest = "authRequest";
+            AuthorizationRequestOIDC4VP authorizationRequestOIDC4VP = AuthorizationRequestOIDC4VP.builder().scope(List.of("scope1")).responseUri("responseUri").state("state").build();
+            CredentialsBasicInfo credentialsBasicInfo = CredentialsBasicInfo.builder().build();
+            VcSelectorRequest expectedVcSelectorRequest = VcSelectorRequest.builder().redirectUri("responseUri").state("state").selectableVcList(List.of(credentialsBasicInfo)).build();
+            when(authorizationRequestService.getJwtRequestObjectFromUri(processId, qrContent)).thenReturn(Mono.just(jwtAuthorizationRequest));
+            when(verifierValidationService.verifyIssuerOfTheAuthorizationRequest(processId, jwtAuthorizationRequest)).thenReturn(Mono.just(jwtAuthorizationRequest));
+            when(authorizationRequestService.getAuthorizationRequestFromJwtAuthorizationRequestJWT(processId, jwtAuthorizationRequest)).thenReturn(Mono.just(authorizationRequestOIDC4VP));
+            when(getUserIdFromToken(authorizationToken)).thenReturn(Mono.just("userId"));
+            when(brokerService.getCredentialByCredentialTypeAndUserId(processId, authorizationRequestOIDC4VP.scope().get(0),"userId")).thenReturn(Mono.just("credentialEntity"));
+            when(dataService.getUserVCsInJson("credentialEntity")).thenReturn(Mono.just(List.of(credentialsBasicInfo)));
+
+            StepVerifier.create(attestationExchangeServiceFacade.processAuthorizationRequest(processId, authorizationToken, qrContent))
+                    .expectNext(expectedVcSelectorRequest)
+                    .verifyComplete();
+        }
+    }
 
     @Test
     void buildVerifiablePresentationWithSelectedVCsTest() throws JsonProcessingException {
         String processId = "123";
         String authorizationToken = "authToken";
-        VcSelectorResponse vcSelectorResponse = VcSelectorResponse.builder().build();
+        String nonce = "321";
+        VcSelectorResponse vcSelectorResponse = VcSelectorResponse.builder().nonce(nonce).build();
         String verifiablePresentation = "vp";
 
         when(presentationService.createSignedVerifiablePresentation(
-                eq(processId),
-                eq(authorizationToken),
-                eq(vcSelectorResponse),
-                anyString(),
-                eq("vpWeb")
+                processId,
+                authorizationToken,
+                vcSelectorResponse,
+                nonce,
+                "https://self-issued.me/v2"
         )).thenReturn(Mono.just(verifiablePresentation));
         when(authorizationResponseService.buildAndPostAuthorizationResponseWithVerifiablePresentation(processId, vcSelectorResponse, verifiablePresentation, authorizationToken)).thenReturn(Mono.empty());
 
--- src/test/java/es/in2/wallet/api/service/AuthorizationRequestOIDC4VPServiceImplTest.java
+++ src/test/java/es/in2/wallet/api/service/AuthorizationRequestOIDC4VPServiceImplTest.java
@@ -1,5 +1,6 @@
 package es.in2.wallet.api.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import es.in2.wallet.domain.model.AuthorizationRequestOIDC4VP;
 import es.in2.wallet.domain.service.impl.AuthorizationRequestServiceImpl;
 import es.in2.wallet.infrastructure.core.config.WebClientConfig;
@@ -15,6 +16,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+
 import static es.in2.wallet.domain.util.ApplicationConstants.CONTENT_TYPE;
 import static es.in2.wallet.domain.util.ApplicationConstants.CONTENT_TYPE_APPLICATION_JSON;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,82 +29,97 @@ class AuthorizationRequestOIDC4VPServiceImplTest {
 
     @Mock
     private WebClientConfig webClientConfig;
+
+    @Mock
+    private ObjectMapper objectMapper;
     @InjectMocks
     private AuthorizationRequestServiceImpl authorizationRequestService;
 
-//    @Test
-//    void getAuthorizationRequestFromVcLoginRequestTest() {
-//            String processId = "123";
-//            String qrContent = "https://example/authentication-requests?state=K00KRn2UT0ydwfs3A-5cZw";
-//            String authorizationToken = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJRQXpUVkRieUxsTVNzX2EwVFdrNy1JbGFDOTRnVjNPaDBDZWlKM2lydUIwIn0.eyJleHAiOjE3MDkwMTkyNTAsImlhdCI6MTcwOTAxODk1MCwianRpIjoiZTY5ZDI0YjQtYzA1OC00MTRmLTg0ZDYtNWZlY2RjMGU1Yzg4IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL3JlYWxtcy9FQUFQcm92aWRlciIsImF1ZCI6InJlYWxtLW1hbmFnZW1lbnQiLCJzdWIiOiIyZWI2NWY0ZC03YzE2LTRiMjUtYjNlZi04NDRlMjg3MmIyMDMiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJvaWRjNHZjaS1jbGllbnQiLCJzZXNzaW9uX3N0YXRlIjoiNGEyNTM2ZGEtYTQxNy00MjUzLWIzNGYtYzcyNjAzNjIzYjZlIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL2VwdS1kZXYtYXBwLTAxLmF6dXJld2Vic2l0ZXMubmV0IiwiaHR0cDovL2xvY2FsaG9zdDo0MjAzIiwiaHR0cHM6Ly9lcHUtZGV2LWFwcC0wMy5henVyZXdlYnNpdGVzLm5ldCIsImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMSIsImh0dHBzOi8vaXNzdWVyaWRwLmRldi5pbjIuZXMiLCJodHRwczovL3dhbGxldGlkcC5kZXYuaW4yLmVzIiwiaHR0cHM6Ly9lcHUtZGV2LWFwcC0wMi5henVyZXdlYnNpdGVzLm5ldCIsImh0dHBzOi8vaXNzdWVyZGV2LmluMi5lcy8qIiwiaHR0cDovL2xvY2FsaG9zdDo0MjAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJhZG1pbiIsImNyZWRlbnRpYWxfcmV2b2NhdGlvbl9hZG1pbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7InJlYWxtLW1hbmFnZW1lbnQiOnsicm9sZXMiOlsiY3JlYXRlLWNsaWVudCIsIm1hbmFnZS11c2VycyIsInZpZXctdXNlcnMiLCJ2aWV3LWNsaWVudHMiLCJ2aWV3LWF1dGhvcml6YXRpb24iLCJtYW5hZ2UtYXV0aG9yaXphdGlvbiIsInF1ZXJ5LWNsaWVudHMiLCJtYW5hZ2UtY2xpZW50cyIsInF1ZXJ5LWdyb3VwcyIsInF1ZXJ5LXVzZXJzIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwic2lkIjoiNGEyNTM2ZGEtYTQxNy00MjUzLWIzNGYtYzcyNjAzNjIzYjZlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiU3VwZXIgQWRtaW4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJpbjJhZG1pbjEiLCJnaXZlbl9uYW1lIjoiU3VwZXIiLCJmYW1pbHlfbmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbnVzZXJAZXhhbXBsZS5jb20ifQ.R9abtB020XMxVwNeco2aUiwB1VjjyIpGdmLEyZXEMz_BAu6bcsohyfkny8I8d8UZ18-Wo2qfgXcZhDwrifGVr3YUnVP_nTJZx5liCM-yYXdnD5CRvSHSLueNk6iiQhE8cHEaJ5Y8oEd0jW0m4ci6XxJd68G5sM_1c3VxLz04mUqZ347n9Cq2B_sXyuN-U95-UAzoHNW23iGy7_Fqe9TCewQEtE7_1xMW3dO8_hCE0O1aUorqkBwWp1FPNQtgQi4bbEHPYI2rJbQXF6yrlagKcmexApiUU93thxulGylX-p64VJP82iw8EOfodiAxWndQk4uuSdVk29uz_AhuOl15fg";
-//
-//            ExchangeFunction exchangeFunction = mock(ExchangeFunction.class);
-//
-//            // Create a mock ClientResponse for a successful response
-//            ClientResponse clientResponse = ClientResponse.create(HttpStatus.OK)
-//                    .header(CONTENT_TYPE, CONTENT_TYPE_APPLICATION_JSON)
-//                    .body("response")
-//                    .build();
-//
-//            // Stub the exchange function to return the mock ClientResponse
-//            when(exchangeFunction.exchange(any())).thenReturn(Mono.just(clientResponse));
-//
-//            WebClient webClient = WebClient.builder().exchangeFunction(exchangeFunction).build();
-//            when(webClientConfig.centralizedWebClient()).thenReturn(webClient);
-//
-//
-//            StepVerifier.create(authorizationRequestService.getAuthorizationRequestFromVcLoginRequest(processId,qrContent))
-//                    .expectNext("response")
-//                    .verifyComplete();
-//
-//    }
-//
-//    @Test
-//    void getAuthorizationRequestFromVcLoginRequestErrorTest() {
-//            String processId = "123";
-//            String qrContent = "qrContentInvalid";
-//            String authorizationToken = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJRQXpUVkRieUxsTVNzX2EwVFdrNy1JbGFDOTRnVjNPaDBDZWlKM2lydUIwIn0.eyJleHAiOjE3MDkwMTkyNTAsImlhdCI6MTcwOTAxODk1MCwianRpIjoiZTY5ZDI0YjQtYzA1OC00MTRmLTg0ZDYtNWZlY2RjMGU1Yzg4IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL3JlYWxtcy9FQUFQcm92aWRlciIsImF1ZCI6InJlYWxtLW1hbmFnZW1lbnQiLCJzdWIiOiIyZWI2NWY0ZC03YzE2LTRiMjUtYjNlZi04NDRlMjg3MmIyMDMiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJvaWRjNHZjaS1jbGllbnQiLCJzZXNzaW9uX3N0YXRlIjoiNGEyNTM2ZGEtYTQxNy00MjUzLWIzNGYtYzcyNjAzNjIzYjZlIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL2VwdS1kZXYtYXBwLTAxLmF6dXJld2Vic2l0ZXMubmV0IiwiaHR0cDovL2xvY2FsaG9zdDo0MjAzIiwiaHR0cHM6Ly9lcHUtZGV2LWFwcC0wMy5henVyZXdlYnNpdGVzLm5ldCIsImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMSIsImh0dHBzOi8vaXNzdWVyaWRwLmRldi5pbjIuZXMiLCJodHRwczovL3dhbGxldGlkcC5kZXYuaW4yLmVzIiwiaHR0cHM6Ly9lcHUtZGV2LWFwcC0wMi5henVyZXdlYnNpdGVzLm5ldCIsImh0dHBzOi8vaXNzdWVyZGV2LmluMi5lcy8qIiwiaHR0cDovL2xvY2FsaG9zdDo0MjAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJhZG1pbiIsImNyZWRlbnRpYWxfcmV2b2NhdGlvbl9hZG1pbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7InJlYWxtLW1hbmFnZW1lbnQiOnsicm9sZXMiOlsiY3JlYXRlLWNsaWVudCIsIm1hbmFnZS11c2VycyIsInZpZXctdXNlcnMiLCJ2aWV3LWNsaWVudHMiLCJ2aWV3LWF1dGhvcml6YXRpb24iLCJtYW5hZ2UtYXV0aG9yaXphdGlvbiIsInF1ZXJ5LWNsaWVudHMiLCJtYW5hZ2UtY2xpZW50cyIsInF1ZXJ5LWdyb3VwcyIsInF1ZXJ5LXVzZXJzIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwic2lkIjoiNGEyNTM2ZGEtYTQxNy00MjUzLWIzNGYtYzcyNjAzNjIzYjZlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiU3VwZXIgQWRtaW4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJpbjJhZG1pbjEiLCJnaXZlbl9uYW1lIjoiU3VwZXIiLCJmYW1pbHlfbmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbnVzZXJAZXhhbXBsZS5jb20ifQ.R9abtB020XMxVwNeco2aUiwB1VjjyIpGdmLEyZXEMz_BAu6bcsohyfkny8I8d8UZ18-Wo2qfgXcZhDwrifGVr3YUnVP_nTJZx5liCM-yYXdnD5CRvSHSLueNk6iiQhE8cHEaJ5Y8oEd0jW0m4ci6XxJd68G5sM_1c3VxLz04mUqZ347n9Cq2B_sXyuN-U95-UAzoHNW23iGy7_Fqe9TCewQEtE7_1xMW3dO8_hCE0O1aUorqkBwWp1FPNQtgQi4bbEHPYI2rJbQXF6yrlagKcmexApiUU93thxulGylX-p64VJP82iw8EOfodiAxWndQk4uuSdVk29uz_AhuOl15fg";
-//
-//            ExchangeFunction exchangeFunction = mock(ExchangeFunction.class);
-//
-//            // Create a mock ClientResponse for a successful response
-//            ClientResponse clientResponse = ClientResponse.create(HttpStatus.BAD_REQUEST)
-//                    .header(CONTENT_TYPE, CONTENT_TYPE_APPLICATION_JSON)
-//                    .body("error")
-//                    .build();
-//
-//            // Stub the exchange function to return the mock ClientResponse
-//            when(exchangeFunction.exchange(any())).thenReturn(Mono.just(clientResponse));
-//
-//            WebClient webClient = WebClient.builder().exchangeFunction(exchangeFunction).build();
-//            when(webClientConfig.centralizedWebClient()).thenReturn(webClient);
-//
-//            StepVerifier.create(authorizationRequestService.getAuthorizationRequestFromVcLoginRequest(processId,qrContent))
-//                    .expectError(RuntimeException.class)
-//                    .verify();
-//
-//    }
+    @Test
+    void getAuthorizationRequestFromVcLoginRequestTest() {
+            String processId = "123";
+            String qrContent = "https://example/authentication-requests?request_uri=https://redirect.com";
+
+            ExchangeFunction exchangeFunction = mock(ExchangeFunction.class);
+
+            // Create a mock ClientResponse for a successful response
+            ClientResponse clientResponse = ClientResponse.create(HttpStatus.OK)
+                    .header(CONTENT_TYPE, CONTENT_TYPE_APPLICATION_JSON)
+                    .body("response")
+                    .build();
+
+            // Stub the exchange function to return the mock ClientResponse
+            when(exchangeFunction.exchange(any())).thenReturn(Mono.just(clientResponse));
+
+            WebClient webClient = WebClient.builder().exchangeFunction(exchangeFunction).build();
+            when(webClientConfig.centralizedWebClient()).thenReturn(webClient);
+
+
+            StepVerifier.create(authorizationRequestService.getJwtRequestObjectFromUri(processId,qrContent))
+                    .expectNext("response")
+                    .verifyComplete();
+
+    }
 
     @Test
-    void getAuthorizationRequestFromJwtAuthorizationRequestClaimTest() {
+    void getAuthorizationRequestFromVcLoginRequestErrorTest() {
+            String processId = "123";
+            String qrContent = "https://example/authentication-requests?request_uri=malformaturl";
+
+
+        ExchangeFunction exchangeFunction = mock(ExchangeFunction.class);
+
+            // Create a mock ClientResponse for a successful response
+            ClientResponse clientResponse = ClientResponse.create(HttpStatus.BAD_REQUEST)
+                    .header(CONTENT_TYPE, CONTENT_TYPE_APPLICATION_JSON)
+                    .body("error")
+                    .build();
+
+            // Stub the exchange function to return the mock ClientResponse
+            when(exchangeFunction.exchange(any())).thenReturn(Mono.just(clientResponse));
+
+            WebClient webClient = WebClient.builder().exchangeFunction(exchangeFunction).build();
+            when(webClientConfig.centralizedWebClient()).thenReturn(webClient);
+
+            StepVerifier.create(authorizationRequestService.getJwtRequestObjectFromUri(processId,qrContent))
+                    .expectError(RuntimeException.class)
+                    .verify();
+
+    }
+
+    @Test
+    void getAuthorizationRequestFromJwtAuthorizationRequestClaimTest() throws Exception {
         String processId = "123";
-        String jwtAuthorizationRequestClaim = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJhdXRoX3JlcXVlc3QiOiJodHRwOi8vZXhhbXBsZS5jb20vP3Njb3BlPWRhdGExLGRhdGEyLGRhdGEzJnJlc3BvbnNlX3R5cGU9dnBfdG9rZW4mcmVzcG9uc2VfbW9kZT1mb3JtX3Bvc3QmY2xpZW50X2lkPW15Q2xpZW50SWQmcmVkaXJlY3RfdXJpPWh0dHA6Ly9leGFtcGxlLmNvbS9yZWRpcmVjdCZzdGF0ZT1teVN0YXRlJm5vbmNlPW15Tm9uY2UifQ.PGO_Wx61hUcTFNndZIFM86KVMVQDl2y0vyUdHWbwvyg";
+        String jwtAuthorizationRequest = "eyJraWQiOiJkaWQ6a2V5OnpEbmFla2l3a1djWG5IYVc2YXUzQnBtZldmcnRWVEpackEzRUhnTHZjYm02RVpudXAiLCJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJyZXNwb25zZV91cmkiOiJodHRwOi8vbG9jYWxob3N0OjkwMDAvb2lkNHZwL2F1dGgtcmVzcG9uc2UiLCJjbGllbnRfaWRfc2NoZW1lIjoiZGlkIiwiaXNzIjoiZGlkOmtleTp6RG5hZWtpd2tXY1huSGFXNmF1M0JwbWZXZnJ0VlRKWnJBM0VIZ0x2Y2JtNkVabnVwIiwicmVzcG9uc2VfdHlwZSI6InZwX3Rva2VuIiwibm9uY2UiOiI4ODU0YWMyNS0yZTFiLTQzYzktYjRiYi0yNmNhY2ZiNTg1ZTIiLCJjbGllbnRfaWQiOiJkaWQ6a2V5OnpEbmFla2l3a1djWG5IYVc2YXUzQnBtZldmcnRWVEpackEzRUhnTHZjYm02RVpudXAiLCJyZXNwb25zZV9tb2RlIjoiZGlyZWN0X3Bvc3QiLCJhdWQiOiJkaWQ6a2V5OnpEbmFla2l3a1djWG5IYVc2YXUzQnBtZldmcnRWVEpackEzRUhnTHZjYm02RVpudXAiLCJzY29wZSI6ImRvbWUuY3JlZGVudGlhbHMucHJlc2VudGF0aW9uLkxFQVJDcmVkZW50aWFsRW1wbG95ZWUiLCJzdGF0ZSI6IjEyMzQ1IiwiZXhwIjoxNzI4ODExNTU4LCJpYXQiOjE3Mjc5NDc1NTh9.WP0FDQXsIjsey9-ktJ5HAJFg9ItuFXL9brZDSIJJuT0YKKz5HpnAY2Dit9cSnZroq9gZnFY_kFm38OOjFUwx6w";
 
-        Mono<AuthorizationRequestOIDC4VP> result = authorizationRequestService.getAuthorizationRequestFromJwtAuthorizationRequestJWT(processId, jwtAuthorizationRequestClaim);
+        // Simulación del payload mapeado desde JWT
+        AuthorizationRequestOIDC4VP expectedAuthorizationRequest = AuthorizationRequestOIDC4VP.builder()
+                .scope(List.of("dome.credentials.presentation.LEARCredentialEmployee"))
+                .responseType("vp_token")
+                .responseMode("direct_post")
+                .clientId("did:key:zDnaekiwkWcXnHaW6au3BpmfWfrtVTJZrA3EHgLvcbm6EZnup")
+                .state("12345")
+                .nonce("8854ac25-2e1b-43c9-b4bb-26cacfb585e2")
+                .build();
 
+        String jsonString = "{\"response_uri\":\"http://localhost:9000/oid4vp/auth-response\",\"client_id_scheme\":\"did\",\"iss\":\"did:key:zDnaekiwkWcXnHaW6au3BpmfWfrtVTJZrA3EHgLvcbm6EZnup\",\"response_type\":\"vp_token\",\"nonce\":\"6a0ad2ea-00f0-498d-b9fd-117a3889d45a\",\"client_id\":\"did:key:zDnaekiwkWcXnHaW6au3BpmfWfrtVTJZrA3EHgLvcbm6EZnup\",\"response_mode\":\"direct_post\",\"aud\":\"did:key:zDnaekiwkWcXnHaW6au3BpmfWfrtVTJZrA3EHgLvcbm6EZnup\",\"scope\":[\"dome.credentials.presentation.LEARCredentialEmployee\"],\"state\":\"12345\",\"exp\":1728818405,\"iat\":1727954405}";
+
+        when(objectMapper.writeValueAsString(any())).thenReturn(jsonString);
+        when(objectMapper.readValue(jsonString, AuthorizationRequestOIDC4VP.class)).thenReturn(expectedAuthorizationRequest);
+
+        Mono<AuthorizationRequestOIDC4VP> result = authorizationRequestService.getAuthorizationRequestFromJwtAuthorizationRequestJWT(processId, jwtAuthorizationRequest);
+
+        // Verificación
         StepVerifier.create(result)
                 .expectNextMatches(authRequest ->
-                        authRequest.scope().contains("data1") &&
-                                authRequest.scope().contains("data2") &&
-                                authRequest.scope().contains("data3") &&
+                        authRequest.scope().contains("dome.credentials.presentation.LEARCredentialEmployee") &&
                                 authRequest.responseType().equals("vp_token") &&
-                                authRequest.responseMode().equals("form_post") &&
-                                authRequest.clientId().equals("myClientId") &&
-                                authRequest.state().equals("myState") &&
-                                authRequest.nonce().equals("myNonce")
+                                authRequest.responseMode().equals("direct_post") &&
+                                authRequest.clientId().equals("did:key:zDnaekiwkWcXnHaW6au3BpmfWfrtVTJZrA3EHgLvcbm6EZnup") &&
+                                authRequest.state().equals("12345") &&
+                                authRequest.nonce().equals("8854ac25-2e1b-43c9-b4bb-26cacfb585e2")
                 )
                 .verifyComplete();
-
     }
 
     @Test
