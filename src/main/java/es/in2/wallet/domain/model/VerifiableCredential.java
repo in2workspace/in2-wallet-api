d9a2bdba17755494fa47cfb0f775031d006e633f
javieralvarezin2 <javier.alvarez@in2.es>
2024-11-12 15:20:28 +0100

    change jsonIgnore to father

--- src/main/java/es/in2/wallet/domain/model/VerifiableCredential.java
+++ src/main/java/es/in2/wallet/domain/model/VerifiableCredential.java
@@ -1,6 +1,7 @@
 package es.in2.wallet.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Builder;
@@ -8,6 +9,7 @@ import lombok.Builder;
 import java.util.List;
 
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record VerifiableCredential(
         @JsonProperty("type")
         List<String> type,
@@ -16,10 +18,10 @@ public record VerifiableCredential(
         @JsonProperty("id")
         String id,
         @JsonProperty("issuer") JsonNode issuer,
-        @JsonIgnore @JsonProperty("issuanceDate") String issuanceDate, // Old Credential version attribute
+        @JsonProperty("issuanceDate") String issuanceDate, // Old Credential version attribute
         @JsonProperty("issued") String issued,
         @JsonProperty("validFrom") String validFrom,
-        @JsonIgnore @JsonProperty("expirationDate") String expirationDate, // Old Credential version attribute
+        @JsonProperty("expirationDate") String expirationDate, // Old Credential version attribute
         @JsonProperty("validUntil") String validUntil, // New Credential version attribute
         @JsonProperty("credentialSubject") JsonNode credentialSubject
 ) {
