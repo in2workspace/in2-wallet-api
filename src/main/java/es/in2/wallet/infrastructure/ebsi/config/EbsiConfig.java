59e5c7f6540be049ea9168d39ba85afa62d0ce56
RubenModamioGarcia <ruben.modamio@in2.es>
2025-02-05 11:58:13 +0100

    update EbsiConfig

--- src/main/java/es/in2/wallet/infrastructure/ebsi/config/EbsiConfig.java
+++ src/main/java/es/in2/wallet/infrastructure/ebsi/config/EbsiConfig.java
@@ -223,6 +223,7 @@ public class EbsiConfig {
         // We'll store it as a plain credential (format=null => code checks "if format == null => parseAsPlainJson(...)")
         CredentialResponse newCredentialResponse = CredentialResponse.builder()
                 .credential(plainJsonVc)
+                .transactionId(UUID.randomUUID().toString())
                 .build();
 
         return userRepositoryService.storeUser(processId, userId)
