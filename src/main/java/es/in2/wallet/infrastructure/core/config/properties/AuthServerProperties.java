6bae667adb5fa3a6ad5baa79cf885a28f366124d
roger <roger.miret@in2.es>
2025-03-20 11:06:15 +0100

    add security prefix to security properties

--- src/main/java/es/in2/wallet/infrastructure/core/config/properties/AuthServerProperties.java
+++ src/main/java/es/in2/wallet/infrastructure/core/config/properties/AuthServerProperties.java
@@ -1,29 +1,19 @@
 package es.in2.wallet.infrastructure.core.config.properties;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
-
-import java.util.Optional;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * AuthServerProperties
  *
  * @param externalUrl - externalUrl auth-server url
  * @param internalUrl - internalUrl auth-server url
- * @param tokenUrl    - token Endpoint
  */
-@ConfigurationProperties(prefix = "auth-server")
-public record AuthServerProperties(@NestedConfigurationProperty UrlProperties externalUrl,
-                                   @NestedConfigurationProperty UrlProperties internalUrl,
-                                   @NestedConfigurationProperty UrlProperties tokenUrl) {
-
-    @ConstructorBinding
-    public AuthServerProperties(UrlProperties externalUrl, UrlProperties internalUrl, UrlProperties tokenUrl) {
-        this.externalUrl = Optional.ofNullable(externalUrl).orElse(new UrlProperties(null, null, null, null));
-        this.internalUrl = Optional.ofNullable(internalUrl).orElse(new UrlProperties(null, null, null, null));
-        this.tokenUrl = Optional.ofNullable(tokenUrl).orElse(new UrlProperties(null, null, null, null));
-
-    }
-
+@Validated
+@ConfigurationProperties(prefix = "security.auth-server")
+public record AuthServerProperties(
+        @NotNull @Valid UrlProperties externalUrl,
+        @NotNull @Valid UrlProperties internalUrl) {
 }
--- src/main/java/es/in2/wallet/infrastructure/vault/adapter/hashicorp/config/properties/HashicorpProperties.java
+++ src/main/java/es/in2/wallet/infrastructure/vault/adapter/hashicorp/config/properties/HashicorpProperties.java
@@ -1,9 +1,14 @@
 package es.in2.wallet.infrastructure.vault.adapter.hashicorp.config.properties;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
-@ConfigurationProperties("hashicorp.vault")
-public record HashicorpProperties(String path, String host, String port, String scheme, String token) {
-
-
+@Validated
+@ConfigurationProperties("security.vault.hashicorp")
+public record HashicorpProperties(
+        @NotNull String host,
+        @NotNull String port,
+        @NotNull String scheme,
+        @NotNull String token) {
 }
