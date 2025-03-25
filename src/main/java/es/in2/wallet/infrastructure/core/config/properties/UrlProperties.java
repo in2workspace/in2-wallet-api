2768ba35a06a6dc913221ce4bf59aed2e311de29
roger <roger.miret@in2.es>
2025-03-25 12:22:31 +0100

    fix properties

--- src/main/java/es/in2/wallet/infrastructure/core/config/properties/UrlProperties.java
+++ src/main/java/es/in2/wallet/infrastructure/core/config/properties/UrlProperties.java
@@ -3,8 +3,6 @@ package es.in2.wallet.infrastructure.core.config.properties;
 import jakarta.validation.constraints.NotNull;
 
 public record UrlProperties(
-        @NotNull String scheme,
         @NotNull String domain,
-        @NotNull Integer port,
         @NotNull String path) {
 }
