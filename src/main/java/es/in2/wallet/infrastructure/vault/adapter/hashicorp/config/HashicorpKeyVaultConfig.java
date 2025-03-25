94d90d6c7e05003c171be7978290db78840cc9d7
roger <roger.miret@in2.es>
2025-03-25 13:33:54 +0100

    add test logs

--- src/main/java/es/in2/wallet/infrastructure/vault/adapter/hashicorp/config/HashicorpKeyVaultConfig.java
+++ src/main/java/es/in2/wallet/infrastructure/vault/adapter/hashicorp/config/HashicorpKeyVaultConfig.java
@@ -3,6 +3,8 @@ package es.in2.wallet.infrastructure.vault.adapter.hashicorp.config;
 import es.in2.wallet.infrastructure.vault.model.VaultProviderEnum;
 import es.in2.wallet.infrastructure.vault.util.VaultProviderAnnotation;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.vault.authentication.ClientAuthentication;
@@ -16,15 +18,22 @@ import org.springframework.vault.config.AbstractReactiveVaultConfiguration;
 public class HashicorpKeyVaultConfig extends AbstractReactiveVaultConfiguration {
 
     private final HashicorpConfig hashicorpConfig;
+    private static final Logger logger = LoggerFactory.getLogger(HashicorpKeyVaultConfig.class);
 
     @Override
     @NonNull
     public VaultEndpoint vaultEndpoint() {
-        VaultEndpoint vaultEndpoint = new VaultEndpoint();
+        String host = hashicorpConfig.getVaultHost();
+        int port = hashicorpConfig.getVaultPort();
+        String scheme = hashicorpConfig.getVaultScheme();
 
-        vaultEndpoint.setHost(hashicorpConfig.getVaultHost());
-        vaultEndpoint.setPort(hashicorpConfig.getVaultPort());
-        vaultEndpoint.setScheme(hashicorpConfig.getVaultScheme());
+//        todo remove
+        logger.info("Initializing VaultEndpoint with host: {}, port: {}, scheme: {}", host, port, scheme);
+
+        VaultEndpoint vaultEndpoint = new VaultEndpoint();
+        vaultEndpoint.setHost(host);
+        vaultEndpoint.setPort(port);
+        vaultEndpoint.setScheme(scheme);
 
         return vaultEndpoint;
     }
@@ -32,7 +41,10 @@ public class HashicorpKeyVaultConfig extends AbstractReactiveVaultConfiguration 
     @Override
     @NonNull
     public ClientAuthentication clientAuthentication() {
-        return new TokenAuthentication(hashicorpConfig.getVaultToken());
+        String token = hashicorpConfig.getVaultToken();
+        // todo remove
+        logger.info("Using Vault token: {}", token != null && !token.isEmpty() ? "[PROVIDED]" : "[MISSING]");
+        return new TokenAuthentication(token);
     }
 
 }
