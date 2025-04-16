cf968e1f031fbf026efd385584f0e0575d3c2799
Victor Poquet <victor.poquet@in2.es>
2025-04-16 13:01:58 +0200

    delete error

--- src/main/java/es/in2/wallet/infrastructure/appconfiguration/exception/WalletUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package es.in2.wallet.infrastructure.appconfiguration.exception;
-
-public class WalletUnavailableException extends RuntimeException {
-    public WalletUnavailableException(String message) {
-        super(message);
-    }
-}
