a64619237e42329c0a0230b67f304e42070034de
Victor Poquet <victor.poquet@in2.es>
2025-04-22 09:26:24 +0200

    logs

--- src/main/java/es/in2/wallet/domain/utils/ApplicationUtils.java
+++ src/main/java/es/in2/wallet/domain/utils/ApplicationUtils.java
@@ -99,6 +99,7 @@ public class ApplicationUtils {
             } catch (Exception e) {
                 log.debug(e.getMessage());
             }
+            log.info(" extractAllQueryParams - PARAMAS: {}",params);
             return params;
         });
     }
