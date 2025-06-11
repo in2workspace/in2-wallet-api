6e9a688bf0148c598014176678e79e6d55550bdc
albalh <alba.lopez@in2.es>
2025-06-11 11:57:53 +0200

    fix v3 fk_credential

--- src/main/resources/db/migration/V3__Modify_CredentialId_Type.sql
+++ src/main/resources/db/migration/V3__Modify_CredentialId_Type.sql
@@ -1,5 +1,11 @@
+ALTER TABLE wallet.deferred_credential_metadata
+    DROP CONSTRAINT fk_credential;
+
 ALTER TABLE wallet.credential
-ALTER COLUMN credential_id TYPE TEXT;
+    ALTER COLUMN credential_id TYPE TEXT;
 
 ALTER TABLE wallet.deferred_credential_metadata
-ALTER COLUMN credential_id TYPE TEXT;
+    ALTER COLUMN credential_id TYPE TEXT;
+
+ALTER TABLE wallet.deferred_credential_metadata
+    ADD CONSTRAINT fk_credential FOREIGN KEY (credential_id) REFERENCES wallet.credential (credential_id) ON DELETE CASCADE;
